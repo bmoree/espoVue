16dbd744340d57c0975808f4287a731535a8cf48
Bob Moree <bmoree@propeller-im.nl>
2023-08-16 23:26:25 +0200

    setup login + metadata call and store data in preferences

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -32,6 +32,9 @@
               <ion-input type="password" v-model="userInfo.password"></ion-input>
             </ion-item>
             <ion-button expand="full" @click="login()">Login</ion-button>
+            <p>Token is: {{ token }}</p>
+            <ion-button expand="full" @click="getMetadata()">Get Metadata</ion-button>
+
           </ion-card-content>
         </ion-card>
       </div>
@@ -40,40 +43,113 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, loadingController } from '@ionic/vue';
 import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
+import { Preferences } from '@capacitor/preferences';
 
-const espoUrl = ref('https://espo.amadeu.ml');
+const espoUrl = ref('http://espocrm.test');
 const userInfo = ref({
   username: '',
   password: ''
 });
+const user = ref({});
+const preferences = ref({});
+const token = ref('');
 
-async function login() {
+ onMounted( () => {
+  setSavedToken()
+  setSavedUser()
+  setSavedPreferences()
+})
+
+
+async function login () {
   const tokenString = btoa(`${userInfo.value.username}:${userInfo.value.password}`)
   await fetch(`${espoUrl.value}/api/v1/App/user`, {
-    // method: "GET", // *GET, POST, PUT, DELETE, etc.
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'include',
+    credentials: "include",
     headers: {
-      "Accept": "application/json, text/javascript, */*; q=0.01",
+      "Accept": " */*",
       "Accept-Encoding": "gzip, deflate, br",
-
+      "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8,nl;q=0.7",
       "Connection": "keep-alive",
       "Content-Type": "application/json",
-      "Host": `${espoUrl.value}`,
+      // "Host": `${espoUrl.value}`,
+      "Host": `http://app.espocrm.test`,
       "Authorization": `Basic ${tokenString}`,
       "Espo-Authorization": `${tokenString}`,
       "Espo-Authorization-By-Token": "false",
       "Espo-Authorization-Create-Token-Secret": "true"
-    }}).then(function(response) {
-        return response.json();
-    }).then(function(){
-        return
-    });
+    }})
+    .then(function(response) {
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data)
+      Preferences.set({
+        key: 'token',
+        value: data.token,
+      });
+      Preferences.set({
+        key: 'user',
+        value: JSON.stringify(data.user),
+      });
+      Preferences.set({
+        key: 'settings',
+        value: JSON.stringify(data.preferences),
+      });
+      
+      setSavedToken()
+      setSavedUser()
+      setSavedPreferences()
+    })
+    .catch(console.error);
 }
+  async function setSavedToken () {
+    const savedToken = await Preferences.get({ key: 'token' })
+    token.value = savedToken.value
+  }
+  async function setSavedUser () {
+    const savedUser = await Preferences.get({ key: 'user' })
+    user.value = JSON.parse(savedUser.value)
+  }
+  async function setSavedPreferences () {
+    const savedPreferences = await Preferences.get({ key: 'preferences' })
+    preferences.value = JSON.parse(savedPreferences.value)
+  }
+
+  async function getMetadata () {
+  const espoToken = btoa(`${userInfo.value.username}:${token.value}`)
+  await fetch(`${espoUrl.value}/api/v1/Metadata`, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "include",
+    headers: {
+      "Accept": " */*",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8,nl;q=0.7",
+      "Connection": "keep-alive",
+      "Content-Type": "application/json",
+      // "Host": `${espoUrl.value}`,
+      "Host": `http://app.espocrm.test`,
+      "Authorization": `Basic ${espoToken}`,
+      "Espo-Authorization": `${espoToken}`,
+      "Espo-Authorization-By-Token": "true",
+      "Espo-Authorization-Create-Token-Secret": "false"
+    }})
+    .then(function(response) {
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data)
+    })
+    .catch(console.error);
+}
+
 </script>
 
 <style scoped>
---
+++ todo.md
@@ -0,0 +1,18 @@
+# simple todo list for this project
+DONE - store token somewhere
+DONE - setup metadata call after login request
+DONE - store metadata
+DONE - store acl
+- set up static routing for account entity
+  - List
+    - show a list of the Accounts
+  - Show (dynamically by id)
+    - show the data for the Entity
+  - Edit
+    - see if we can maybe setup the simplest edit page ever.
+- add field components for show/edit
+- populate show/edit pages based on metadata
+  - entitydefs 
+  - layouts
+- add bottom panel support for related entities
+- add navigation to all allowed Entities
